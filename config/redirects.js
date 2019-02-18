a598a95d4e3cde95314e3bb8f5b857ba1d4b18b0
Katie Horne <katie@auth0.com>
2019-02-18 11:31:34 -0800

    Update redirect.js

--- config/redirects.js
+++ config/redirects.js
@@ -2040,6 +2040,9 @@ module.exports = [
    {
      from: '/users/redirecting-users',
      to: '/users/guides/redirect-users-after-login'
-   }
+   },
+   { from: '/analytics/integrations/facebook-analytics', to: '/' },
+   { from: '/analytics/integrations/google-analytics', to: '/' },
+   { from: '/analytics/integrations', to: '/' }
 ];
 
