9cac650cd06793a31022ca9a64d23d4c76d25eb6
Rachel Khoriander <rachel.khoriander@auth0.com>
2020-09-20 20:57:48 -0500

    Change platform to look at GitHub rather than Contentful for SSO Integrations

--- config/redirects.js
+++ config/redirects.js
@@ -2022,80 +2022,80 @@ module.exports = [
     to: '/integrations/sharepoint-2010-2013'
   },
   {
-    from: ['/sso/current/integrations', '/integrations/sso'],
-    to: '/integrations/sso-integrations'
+    from: ['/integrations/sso-integrations','/sso/current/integrations'],
+    to: '/integrations/sso'
   },
   {
-    from: ['/sso/current/integrations/ad-rms', '/integrations/sso/ad-rms'],
-    to: '/integrations/sso-integrations/ad-rms-sso-integration'
+    from: ['/integrations/sso-integrations/ad-rms-sso-integration','/sso/current/integrations/ad-rms'],
+    to: '/integrations/sso/ad-rms'
   },
   {
-    from: ['/sso/current/integrations/box', '/integrations/sso/box'],
-    to: '/integrations/sso-integrations/box'
+    from: ['/integrations/sso-integrations/box','/sso/current/integrations/box'],
+    to: '/integrations/sso/box'
   },
   {
-    from: ['/sso/current/integrations/cloudbees', '/integrations/sso/cloudbees'],
-    to: '/integrations/sso-integrations/cloudbees'
+    from: ['/integrations/sso-integrations/cloudbees','/sso/current/integrations/cloudbees'],
+    to: '/integrations/sso/cloudbees'
   },
   {
-    from: ['/sso/current/integrations/concur', '/integrations/sso/concur'],
-    to: '/integrations/sso-integrations/concur'
+    from: ['/integrations/sso-integrations/concur','/sso/current/integrations/concur'],
+    to: '/integrations/sso/concur'
   },
   {
-    from: ['/sso/current/integrations/disqus', '/integrations/sso/disqus','/integrations/disqus'],
-    to: '/integrations/sso-integrations/disqus'
+    from: ['/integrations/sso-integrations/disqus','/sso/current/integrations/disqus','/integrations/disqus'],
+    to: '/integrations/sso/disqus'
   },
   {
-    from: ['/sso/current/integrations/dropbox', '/integrations/sso/dropbox','/integrations/dropbox'],
-    to: '/integrations/sso-integrations/dropbox'
+    from: ['/integrations/sso-integrations/dropbox','/sso/current/integrations/dropbox','/integrations/dropbox'],
+    to: '/integrations/sso/dropbox'
   },
   {
-    from: ['/sso/current/integrations/dynamics-crm', '/integrations/sso/dynamics-crm','/integrations/dynamics-crm'],
-    to: '/integrations/sso-integrations/dynamics-crm'
+    from: ['/integrations/sso-integrations/dynamics-crm','/sso/current/integrations/dynamics-crm','/integrations/dynamics-crm'],
+    to: '/integrations/sso/dynamics-crm'
   },
   {
-    from: ['/sso/current/integrations/echosign', '/integrations/sso/echosign','/integrations/echosign'],
-    to: '/integrations/sso-integrations/echosign'
+    from: ['/integrations/sso-integrations/echosign','/sso/current/integrations/echosign','/integrations/echosign'],
+    to: '/integrations/sso/adobe-sign'
   },
   {
-    from: ['/sso/current/integrations/egnyte', '/integrations/sso/egnyte','/integrations/egnyte'],
-    to: '/integrations/sso-integrations/egnyte'
+    from: ['/integrations/sso-integrations/egnyte','/sso/current/integrations/egnyte','/integrations/egnyte'],
+    to: '/integrations/sso/egnyte'
   },
   {
-    from: ['/sso/current/integrations/new-relic', '/integrations/sso/new-relic','/integrations/new-relic'],
-    to: '/integrations/sso-integrations/new-relic'
+    from: ['/integrations/sso-integrations/new-relic','/sso/current/integrations/new-relic','/integrations/new-relic'],
+    to: '/integrations/sso/new-relic'
   },
   {
-    from: ['/sso/current/integrations/office-365', '/integrations/sso/office-365','/integrations/office-365'],
-    to: '/integrations/sso-integrations/office-365'
+    from: ['/integrations/sso-integrations/office-365','/sso/current/integrations/office-365','/integrations/office-365'],
+    to: '/integrations/sso/office-365'
   },
   {
-    from: ['/sso/current/integrations/salesforce', '/integrations/sso/salesforce','/integrations/salesforce'],
-    to: '/integrations/sso-integrations/salesforce'
+    from: ['/integrations/sso-integrations/salesforce','/sso/current/integrations/salesforce','/integrations/salesforce'],
+    to: '/integrations/sso/salesforce'
   },
   {
-    from: ['/sso/current/integrations/slack', '/integrations/sso/slack','/integrations/integrating-with-slack','/tutorials/integrating-with-slack','/scenarios/slack','/integrations/slack'],
-    to: '/integrations/sso-integrations/slack'
+    from: ['/integrations/sso-integrations/slack','/sso/current/integrations/slack','/integrations/integrating-with-slack','/tutorials/integrating-with-slack','/scenarios/slack','/integrations/slack'],
+    to: '/integrations/sso/slack'
   },
   {
-    from: ['/sso/current/integrations/sentry', '/integrations/sso/sentry','/integrations/sentry'],
-    to: '/integrations/sso-integrations/sentry'
+    from: ['/integrations/sso-integrations/sentry','/sso/current/integrations/sentry','/integrations/sentry'],
+    to: '/integrations/sso/sentry'
   },
   {
-    from: ['/sso/current/integrations/sharepoint', '/integrations/sso/sharepoint'],
-    to: '/integrations/sso-integrations/sharepoint'
+    from: ['/integrations/sso-integrations/sharepoint','/sso/current/integrations/sharepoint'],
+    to: '/integrations/sso/sharepoint'
   },
   {
-    from: ['/sso/current/integrations/springcm','/integrations/sso/springcm','/integrations/springcm'],
-    to: '/integrations/sso-integrations/springcm'
+    from: ['/integrations/sso-integrations/springcm','/sso/current/integrations/springcm','/integrations/springcm'],
+    to: '/integrations/sso/springcm'
   },
   {
-    from: ['/sso/current/integrations/zendesk', '/integrations/sso/zendesk','/integrations/zendesk'],
-    to: '/integrations/sso-integrations/zendesk'
+    from: ['/integrations/sso-integrations/zendesk','/sso/current/integrations/zendesk','/integrations/zendesk'],
+    to: '/integrations/sso/zendesk'
   },
   {
-    from: ['/sso/current/integrations/zoom', '/integrations/sso/zoom','/integrations/zoom'],
-    to: '/integrations/sso-integrations/zoom'
+    from: ['/integrations/sso-integrations/zoom','/sso/current/integrations/zoom','/integrations/zoom'],
+    to: '/integrations/sso/zoom'
   },
   {
     from: ['/integrations/azure-tutorial','/azure-tutorial','/tutorials/azure-tutorial','/integrations/azure-api-management/configure-auth0','/integrations/azure-api-management/configure-azure'],
