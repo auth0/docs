03847a8df6b640753f72f4fa24f0786363cea1eb
Amanda VS <amanda.vanscoy@auth0.com>
2021-07-14 13:47:06 -0400

    Update redirects.js
    Updated redirect

--- config/redirects.js
+++ config/redirects.js
@@ -2501,8 +2501,8 @@ module.exports = [
     to: '/integrations/sso/freshdesk'
   },
   {
-    from: ['/integrations/sso-integrations/g-suite'],
-    to: '/integrations/sso/g-suite'
+    from: ['/integrations/sso-integrations/g-suite','/integrations/sso/g-suite'],
+    to: '/integrations/sso/google-workspace'
   },
   {
     from: ['/integrations/sso-integrations/github-enterprise-cloud'],
