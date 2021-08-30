f3b1787e53e9787e1b85694a6e5247921bb19924
Rachel Khoriander <rachel.khoriander@auth0.com>
2021-08-30 13:53:11 -0500

    Remove old oidc-auth redirect

--- config/redirects.js
+++ config/redirects.js
@@ -4671,10 +4671,6 @@ module.exports = [
     to: '/login'
   },
   {
-    from: ['/login/oidc-auth'],
-    to: '/login/oidc-auth/adopt-oidc-conformant-authentication'
-  },
-  {
     from: [
       '/hosted-pages/hosted-login-auth0js',
       '/hosted-pages/login/auth0js',
