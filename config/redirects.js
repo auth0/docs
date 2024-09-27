757055c5015cd227661872109978dd6af8239219
Andrew <109249228+pavokta@users.noreply.github.com>
2024-09-27 13:49:19 -0700

    Update redirects.js
    Removing erroneous redirect

--- config/redirects.js
+++ config/redirects.js
@@ -8863,7 +8863,6 @@ const redirects = [
     from: [
       '/users/search/v3/query-syntax',
       '/users/user-search/user-search-query-syntax',
-      '/manage-users/user-search/user-search-query-syntax',
     ],
     to: '/manage-users/user-search',
   },
